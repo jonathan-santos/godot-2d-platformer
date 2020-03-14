68cc4ab40b8955a1ae924951a786cdbb70afa76d
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-03-14 17:49:27 -0300

    Added player sprites and added Animation playing and flipping

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,9 +6,16 @@ var speed = 12000
 func _process(delta):
 	vel = Vector2()
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		vel.x += speed
-	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("move_left"):
 		vel.x -= speed
+		$AnimatedSprite.flip_h = true
+	
+	if(vel.x != 0):
+		$AnimatedSprite.play("move")
+	else:
+		$AnimatedSprite.play("idle")	
 		
 	move_and_slide(vel * delta, Vector2.UP)
